a18e2487a86a82b041878deb239e732a8594667b
asaow <asa.widerstrand@gmail.com>
2016-04-21 16:23:13 +0200

    Kommentarer, städning FlightFx

--- src/flightfx/FlightFx.java
+++ src/flightfx/FlightFx.java
@@ -1,36 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package flightfx;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
 /**
+ * FlightFx innehåller mainmetoden.
  *
- * @author Loki
+ * @author Grupp 2
  */
 public class FlightFx extends Application {
-    
+
     public static Client client;
 
-    
     @Override
     public void start(Stage stage) throws IOException {
-        
-        client = ClientBuilder.newClient();    
+
+        client = ClientBuilder.newClient();
 
         Parent root = FXMLLoader.load(getClass().getResource("StartScene.fxml"));
         Scene scene = new Scene(root);
@@ -45,5 +36,5 @@ public class FlightFx extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
